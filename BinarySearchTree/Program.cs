3e9e24d0e9893df8dff0af67e092196b1a6990e7
Shivani21-hude <shivani21hude@gmail.com>
2023-01-23 22:10:28 +0530

    [Shivani]-Adding elements in BST

---
+++ BinarySearchTree/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinarySearchTree
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            
+            binarySearch.Display();
+            bool result = binarySearch.IfExists(67, binarySearch);
+            Console.WriteLine(result);
+            Console.ReadKey();
+
+        }
+    }
+}
